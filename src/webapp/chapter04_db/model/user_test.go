d1e8c83ab2f6b490b2b966c5346fc899b55ff704
ifamec <famousfame@qq.com>
2021-02-18 21:57:58 -0800

    update unit test

--- src/webapp/chapter04_db/model/user_test.go
+++ src/webapp/chapter04_db/model/user_test.go
@@ -5,8 +5,23 @@ import (
 	"testing"
 )
 
-func TestUser_AddUser(t *testing.T) {
-	fmt.Println("Test AddUser")
+// TestMain actions before all tests
+func TestMain(m *testing.M)  {
+	fmt.Println("Before All Tests")
+
+	// run Tests
+	m.Run()
+}
+
+func TestUser(t *testing.T)  {
+	fmt.Println("Start User model Test")
+
+	// run child test
+	t.Run("Test Add User", testAddUser)
+}
+
+func testAddUser(t *testing.T) {
+	// fmt.Println("Test AddUser")
 	user := &User{}
 
 	_ = user.AddUser()
