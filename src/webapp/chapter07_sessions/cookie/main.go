034a04df29dae424093ddb33c96e79ef31277f22
ifamec <famousfame@qq.com>
2021-03-05 21:29:36 -0800

    session - cookie - set/get

--- src/webapp/chapter07_sessions/cookie/main.go
+++ src/webapp/chapter07_sessions/cookie/main.go
@@ -1,28 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // set cookie
 func setCookie(w http.ResponseWriter, r *http.Request)  {
 	cookie := http.Cookie{
-		Name:       "user",
-		Value:      "admin",
-		HttpOnly:   true,
+		Name:     "user",
+		Value:    "admin",
+		HttpOnly: true,
+		MaxAge:   60,
 	}
-	// cookie_2 := http.Cookie{
-	// 	Name:       "user",
-	// 	Value:      "admin",
-	// 	HttpOnly:   true,
-	// }
+	cookie2 := http.Cookie{
+		Name:     "user1",
+		Value:    "user01",
+		HttpOnly: true,
+	}
 	// w.Header().Set("Set-Cookie", cookie.String())
 	// w.Header().Add("Set-Cookie", cookie_2.String())
 
 	http.SetCookie(w, &cookie)
+	http.SetCookie(w, &cookie2)
+}
+
+// get cookies
+func getCookies(w http.ResponseWriter, r *http.Request) {
+	// cookies := r.Header["Cookie"]
+	cookies := r.Cookies()
+	fmt.Println(cookies)
+	c1, _ := r.Cookie("user")
+	c2, _ := r.Cookie("user1")
+	fmt.Println(c1, c2)
 }
 
 func main()  {
 	http.HandleFunc("/setCookie", setCookie)
+	http.HandleFunc("/getCookies", getCookies)
 	_ = http.ListenAndServe(":8000", nil)
 }
